7e63539a6f1bd1bca03a5b127949628748d33df0
Parth <parthp15@yahoo.com>
2024-06-14 22:35:58 -0700

    marquee done

--- design-project/src/App.jsx
+++ design-project/src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-zinc-900 text-white">
+    <div className="w-full min-h-screen bg-zinc-900 text-white">
       <Navbar />
       <LandingPage />
+      <Marquee />
     </div>
   );
 }
