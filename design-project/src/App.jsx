3e5d8f0bb0daca67b208e91a3775c17621ce0b8c
Parth <parthp15@yahoo.com>
2024-06-18 09:46:58 -0700

    eyes done

--- design-project/src/App.jsx
+++ design-project/src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Eyes from "./components/Eyes";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ function App() {
       <LandingPage />
       <Marquee />
       <About />
+      <Eyes />
     </div>
   );
 }
