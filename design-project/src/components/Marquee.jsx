70ce63e8d2a52991a2f440a83d59d71e2b078473
Parth <parthp15@yahoo.com>
2024-06-18 12:01:43 -0700

    marquee animation done

--- design-project/src/components/Marquee.jsx
+++ design-project/src/components/Marquee.jsx
@@ -3,20 +3,20 @@ import { motion } from "framer-motion";
 function Marquee() {
   return (
     <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 whitespace-nowrap">
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap">
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
-          className="text-[17vw] leading-none font-['Founders_Grotesk_X-Condensed'] uppercase pt-7 -mb-[4.6vw] font-semibold"
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[24vw] leading-none font-['Founders_Grotesk_X-Condensed'] uppercase pt-7 -mb-[7vw] font-semibold pr-20"
         >
           We are ochi
         </motion.h1>
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
-          className="text-[17vw] leading-none font-['Founders_Grotesk_X-Condensed'] uppercase pt-7 -mb-[4.6vw] font-semibold"
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[24vw] leading-none font-['Founders_Grotesk_X-Condensed'] uppercase pt-7 -mb-[7vw] font-semibold pr-20"
         >
           We are ochi
         </motion.h1>
